bd7426f7e84c9bf03fd37dfffc6372c8d825c0bb
AlexCanCode <AlexCanCode18@gmail.com>
2018-11-26 12:41:27 -0700

    added routing and finished header component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 /* JSX Notes
 	- Indentation: difficult in JSX because of the return call on the elemens.
@@ -24,11 +25,24 @@ import React from 'react';
 */ 
 
 class StorePicker extends React.Component {
+	myInput = React.createRef();
+
+	goToStore = (event) => {
+		event.preventDefault();
+		const storeName = this.myInput.current.value;
+		this.props.history.push(`/store/${storeName}`)
+	}
 	render() {
 		return (
-		<form className="store-selector">
+		<form className="store-selector" onSubmit={this.goToStore}>
 			<h2>Please Enter a Store</h2>
-			<input type="text" required placeholder="Store Name" />
+			<input 
+			type="text" 
+			required 
+			ref={this.myInput}
+			placeholder="Store Name" 
+			defaultValue={getFunName()}
+			/>
 			<button type="submit">Visit Store --></button>
 		</form>
 		)
