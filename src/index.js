9bc83a802a14af3d399674d310d77262080ccdb9
AlexCanCode <AlexCanCode18@gmail.com>
2018-11-26 12:42:24 -0700

    added routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react'; //Go into the node_modules folder and import react
 import { render } from 'react-dom'; //Required to render to DOM -- we only need the render method 
-import StorePicker from './components/StorePicker';
 import App from './components/App';
 import "./css/style.css"; //webpack can understand that this is css and will hot reload when it detects changes 
-
-//All classes in react need at least 1 method in it --> render()
-
+import Router from './components/Router'
 
 
-render(<App />, document.querySelector('#main'));
+
+render(<Router />, document.querySelector('#main'));
