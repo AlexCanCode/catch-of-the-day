8e55cbd8c40ad5b459cdc4a959398347d2296e33
AlexCanCode <AlexCanCode18@gmail.com>
2018-11-25 18:26:52 -0700

    add components to app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-// let's go!
+import React from 'react'; //Go into the node_modules folder and import react
+import { render } from 'react-dom'; //Required to render to DOM -- we only need the render method 
+import StorePicker from './components/StorePicker';
+import App from './components/App';
+import "./css/style.css"; //webpack can understand that this is css and will hot reload when it detects changes 
+
+//All classes in react need at least 1 method in it --> render()
+
+
+
+render(<App />, document.querySelector('#main'));
