6eb9f76b7b16a23f8f785c6e55713c0cafc25569
AlexCanCode <AlexCanCode18@gmail.com>
2018-11-27 10:04:26 -0700

    passed state up from addfishform to App.js, setState to update

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,21 @@ import Order from "./Order"
 import Inventory from "./Inventory"
 
 class App extends React.Component {
+	state = {
+		fishes: {},
+		order: {}
+	};
+	addFish = (fish) => {
+		//updating state in React 
+		//1. Take a copy of the existing state - want to avoid mutating the state object
+		const fishes = { ...this.state.fishes };
+		//2. Update the copy of the state (uodate fishes)
+		fishes[`fish${Date.now()}`] = fish;
+		//3. Set the new fishes object to state
+		this.setState({
+			fishes: fishes //in ES6 could just go ({ fishes })
+		})
+	};
 	render() {
 		return (
 			<div className="catch-of-the-day">
@@ -11,7 +26,7 @@ class App extends React.Component {
 					<Header tagline="Fresh Seafood Market"/>
 				</div>
 				<Order />
-				<Inventory />
+				<Inventory addFish={this.addFish} />
 			</div>
 		);
 	}
