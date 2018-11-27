e218f1c3a6a3271dd082e62f91a7538b7cb5092a
AlexCanCode <AlexCanCode18@gmail.com>
2018-11-27 10:05:00 -0700

    changed event to e for consistency

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -27,8 +27,8 @@ import { getFunName } from '../helpers';
 class StorePicker extends React.Component {
 	myInput = React.createRef();
 
-	goToStore = (event) => {
-		event.preventDefault();
+	goToStore = (e) => {
+		e.preventDefault();
 		const storeName = this.myInput.current.value;
 		this.props.history.push(`/store/${storeName}`)
 	}
